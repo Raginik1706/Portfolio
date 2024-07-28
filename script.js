e4b41060a3f3f2bbf203e045767ad37dbfdb156f
raginik1706 <raginikumari17062002@gmail.com>
2024-07-28 20:14:50 +0530

    add all files

---
+++ script.js
@@ -0,0 +1,50 @@
+var a = gsap.timeline();
+
+a
+.from(".img" ,{
+    opacity:0,
+    duration: 2
+
+})
+.from(".profile-text" ,{
+    opacity:0,
+    duration: 2
+
+})
+.from(".port_logo " ,{
+      opacity:0,
+      duration: 1
+})
+.from(".port-text " ,{
+    opacity:0,
+    duration: 1
+
+})
+.from(".folio-text " ,{
+    opacity:0,
+    duration: 1
+
+})
+.from(".hello" ,{
+    opacity:0,
+    duration: 1
+
+})
+.from(".ragini" ,{
+  opacity:0,
+  duration: 1
+
+})
+.from(".cv" ,{
+  opacity:0,
+  duration: 1
+
+})
+
+.from(".nav-items li" ,{
+      opacity:0,
+      duration: 1
+
+})
+
+
